799dafbdce4b3f2e38b9ae37f6a2c6fe5f8c314d
sukekyo000 <sukekyo000@gmail.com>
2022-12-13 20:55:05 +0900

    A Answer

---
+++ ABC225/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+
+    cin >> S;
+    
+    if(S[0] == S[1] && S[1] == S[2]){
+        cout << 1 << endl;
+    } else if (S[0] == S[1] || S[1] == S[2] || S[2] == S[0]) {
+        cout << 3 << endl;
+    } else {
+        cout << 6 << endl;
+    }
+
+    return 0;
+}
