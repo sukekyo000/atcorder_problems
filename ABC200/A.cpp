3762c1e745b3bed3bbba620aa917797a16f267be
sukekyo000 <sukekyo000@gmail.com>
2022-12-19 21:31:59 +0900

    A Answer

---
+++ ABC200/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    if(N % 100 == 0){
+        cout << (N / 100) << endl;
+    } else {
+        cout << (N / 100) + 1 << endl;
+    }
+
+    return 0;
+}
