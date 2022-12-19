e68d91fb26a8ffc5944c41e1d6be0c5d1dae6d9e
sukekyo000 <sukekyo000@gmail.com>
2022-12-19 21:40:31 +0900

    A Answer

---
+++ ABC195/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, H;
+
+    cin >> M >> H;
+
+    if(H % M == 0){
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
