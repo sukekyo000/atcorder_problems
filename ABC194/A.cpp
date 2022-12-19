058fab7669da31b588ec1c5632798c1897e3f369
sukekyo000 <sukekyo000@gmail.com>
2022-12-19 21:41:32 +0900

    A Answer

---
+++ ABC194/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+
+    cin >> A >> B;
+
+    if(A + B >= 15 && B >= 8){
+        cout << 1 << endl;
+    } else if (A + B >= 10 && B >= 3){
+        cout << 2 << endl;
+    } else if (A + B >= 3){
+        cout << 3 << endl;
+    } else {
+        cout << 4 << endl;
+    }
+
+    return 0;
+}
