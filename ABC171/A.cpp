e12e6c95baae8caefb5e1368d0c985ccaf48c07c
sukekyo000 <sukekyo000@gmail.com>
2022-12-21 21:21:46 +0900

    A Answer

---
+++ ABC171/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char a;
+
+    cin >> a;
+
+    if(isupper(a)){
+        cout << "A" << endl;
+    } else {
+        cout << "a" << endl;
+    }
+
+    return 0;
+}
